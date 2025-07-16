8e6463e646c8cab6f1775f463667da783319d7c8
Fedor Guchinskii <gucinskijfedor@gmail.com>
2025-07-17 02:14:33 +0300

    Настроил аугментацию

--- src/data_manager.py
+++ src/data_manager.py
@@ -11,4 +11,15 @@ class Cifar10DataManager:
         
         self.class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                               'dog', 'frog', 'horse', 'ship', 'truck']
-      
+        
+        self._build_augmentation_pipeline()
+
+    
+    def _build_augmentation_pipeline(self):
+        self.data_augmentation = tf.keras.Sequential([
+            tf.keras.layers.RandomFlip("horizontal", input_shape=(config.IMAGE_HEIGHT, config.IMAGE_WIDTH, config.IMAGE_CHANNELS)),
+            tf.keras.layers.RandomRotation(0.1),
+            tf.keras.layers.RandomZoom(0.1),
+        ])
+
+    
